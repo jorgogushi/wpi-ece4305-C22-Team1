23ca9466004a6b31b25ce177c293114019fbfd2e
Jorgo Gushi <89947053+jorgogushi@users.noreply.github.com>
2022-02-13 21:33:37 -0500

    Update ece4305_rev1.py

--- ece4305_rev1.py
+++ ece4305_rev1.py
@@ -24,64 +24,46 @@ f = np.fft.fftshift(f)+fc
 Sxx = np.fft.fftshift(Sxx, axes=0,)
 Sxx = np.transpose(Sxx)
 Sxx = np.flipud(Sxx)
-# data = np.abs(data_array)
-# phase = np.angle(data_array)
-
-# #doesn't work becasue you're changing the whole data array
-# for i in phase:
-#    if i < 0:
-#       data = np.abs(data_array)*(-1)
-#    else:
-#       data = np.abs(data_array)*(+1)
-
-# for i in range(len(phase)):
-#     if phase[i] < 0:
-#         data[i] = data[i] * -1
-#     else:
-#         data[i] = data[i] * 1
-
-# for i in range(len(phase)):
-#     if phase[i] < 0:
-#         data[i] = data[i] * -1
-    
-# out_data = []
-# for i in data_array:
-#     out_data.append(np.abs(i)*np.sign(np.phase(i)))
 
 out_data = [np.abs(x)*np.sign(np.angle(x)) for x in data_array]
 time_domain = np.linspace(0,sdr.rx_buffer_size/sample_rate,len(data_array))
 
-Fs = 300 # sample rate
-Ts = 1/Fs # sample period
-N = 2048 # number of samples to simulate
-
-t = Ts*np.arange(N)
-x = np.exp(1j*2*np.pi*50*t) # simulates sinusoid at 50 Hz
-
-n = (np.random.randn(N) + 1j*np.random.randn(N))/np.sqrt(2) # complex noise with unity power
-noise_power = 2
-r = x + n * np.sqrt(noise_power)
-
-PSD = (np.abs(np.fft.fft(r))/N)**2
-PSD_log = 10.0*np.log10(PSD)
-PSD_shifted = np.fft.fftshift(PSD_log)
-
-f = np.arange(Fs/-2.0, Fs/2.0, Fs/N) # start, stop, step
-
-plt.subplot(3,1,1)
-plt.plot(f, PSD_shifted)
-plt.xlabel("Frequency [Hz]")
-plt.ylabel("Magnitude [dB]")
-plt.grid(True)
-plt.show()
-
-plt.subplot(3,1,2)
+plt.subplot(2,1,1)
 plt.plot(time_domain, out_data)
 plt.xlabel("Time [sec]")
 plt.ylabel("Magnitude")
 
-plt.subplot(3,1,3)
+plt.subplot(2,1,2)
 plt.pcolormesh(f, t, Sxx, shading="gouraud")
 plt.xlabel("Freqeuency [Hz]")
 plt.ylabel("Time [sec]")
 plt.show()
+
+mu = 0 # initial estimate of phase of sample
+out = np.zeros(len(samples) + 10, dtype=np.complex)
+out_rail = np.zeros(len(samples) + 10, dtype=np.complex) # stores values, each iteration we need the previous 2 values plus current value
+i_in = 0 # input samples index
+i_out = 2 # output index (let first two outputs be 0)
+while i_out < len(samples) and i_in < len(samples):
+    out[i_out] = samples[i_in + int(mu)] # grab what we think is the "best" sample
+    out_rail[i_out] = int(np.real(out[i_out]) > 0) + 1j*int(np.imag(out[i_out]) > 0)
+    x = (out_rail[i_out] - out_rail[i_out-2]) * np.conj(out[i_out-1])
+    y = (out[i_out] - out[i_out-2]) * np.conj(out_rail[i_out-1])
+    mm_val = np.real(y - x)
+    mu += sps + 0.3*mm_val
+    i_in += int(np.floor(mu)) # round down to nearest int since we are using it as an index
+    mu = mu - np.floor(mu) # remove the integer part of mu
+    i_out += 1 # increment output index
+out = out[2:i_out] # remove the first two, and anything after i_out (that was never filled out)
+samples = out 
+
+samples = samples**2
+psd = np.fft.fftshift(np.abs(np.fft.fft(samples)))
+f = np.linspace(-fs/2.0, fs/2.0, len(psd))
+plt.plot(f, psd)
+plt.show()
+
+max_freq = f[np.argmax(psd)]
+Ts = 1/fs # calc sample period
+t = np.arange(0, Ts*len(samples), Ts) # create time vector
+samples = samples * np.exp(-1j*2*np.pi*max_freq*t/2.0)
