cef5b1ad2277085ec4d64a0238a27e8792a19563
Jorgo Gushi <89947053+jorgogushi@users.noreply.github.com>
2022-02-13 23:59:35 -0500

    Update ece4305_rev1.py

--- ece4305_rev1.py
+++ ece4305_rev1.py
@@ -39,26 +39,9 @@ plt.xlabel("Freqeuency [Hz]")
 plt.ylabel("Time [sec]")
 plt.show()
 
-mu = 0 # initial estimate of phase of sample
-out = np.zeros(len(samples) + 10, dtype=np.complex)
-out_rail = np.zeros(len(samples) + 10, dtype=np.complex) # stores values, each iteration we need the previous 2 values plus current value
-i_in = 0 # input samples index
-i_out = 2 # output index (let first two outputs be 0)
-while i_out < len(samples) and i_in < len(samples):
-    out[i_out] = samples[i_in + int(mu)] # grab what we think is the "best" sample
-    out_rail[i_out] = int(np.real(out[i_out]) > 0) + 1j*int(np.imag(out[i_out]) > 0)
-    x = (out_rail[i_out] - out_rail[i_out-2]) * np.conj(out[i_out-1])
-    y = (out[i_out] - out[i_out-2]) * np.conj(out_rail[i_out-1])
-    mm_val = np.real(y - x)
-    mu += sps + 0.3*mm_val
-    i_in += int(np.floor(mu)) # round down to nearest int since we are using it as an index
-    mu = mu - np.floor(mu) # remove the integer part of mu
-    i_out += 1 # increment output index
-out = out[2:i_out] # remove the first two, and anything after i_out (that was never filled out)
-samples = out 
-
-samples = samples**2
-psd = np.fft.fftshift(np.abs(np.fft.fft(samples)))
+#Coarse Frequency Correction
+data_array = data_array**2
+psd = np.fft.fftshift(np.abs(np.fft.fft(data_array)))
 f = np.linspace(-fs/2.0, fs/2.0, len(psd))
 plt.plot(f, psd)
 plt.show()
@@ -66,4 +49,6 @@ plt.show()
 max_freq = f[np.argmax(psd)]
 Ts = 1/fs # calc sample period
 t = np.arange(0, Ts*len(samples), Ts) # create time vector
-samples = samples * np.exp(-1j*2*np.pi*max_freq*t/2.0)
+data_array = data_array * np.exp(-1j*2*np.pi*max_freq*t/2.0)
+
+#Fine Frequency Correction
