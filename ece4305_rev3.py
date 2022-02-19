a98db3c551c4d626ac9f66283378ca57f67cb693
eewilliams-wpi <65095584+eewilliams-wpi@users.noreply.github.com>
2022-02-19 13:58:04 -0500

    Includes some more sample code from Wyglinski.
    I think my PED works but I'm not sure how to implement the LPF and NCO.

--- ece4305_rev3.py
+++ ece4305_rev3.py
@@ -35,6 +35,31 @@ data_array_fft_shifted= np.abs(np.fft.fftshift(np.fft.fft((data_array))))
 
 
 #Coarse Frequency Correction
+
+#Plot time domain and spectrogram
+
+#plt.subplot(2,1,1)
+#plt.plot(time_domain, out_data)
+#plt.xlabel("Time [sec]")
+#plt.ylabel("Magnitude")
+
+#plt.subplot(2,1,2)
+#plt.pcolormesh(f, t, Sxx, shading="gouraud")
+#plt.xlabel("Freqeuency [Hz]")
+#plt.ylabel("Time [sec]")
+#plt.show()
+
+# This is for testing only
+#print(offset)
+#samples_of_f_1 = np.abs(np.fft.fftshift(np.fft.fft((samples_shifted))))
+#fig, (plotT, plotF) = plt.subplots(2)
+#plotT.plot(freq_domain,  samples_of_f_1)
+#plotF.plot(freq_domain, data_array_fft_shifted)
+#plt.show()
+
+#Fine Frequency Correction
+
+#DPLL
 #PED
 
 #Ideal FSK from Wyglinski
@@ -61,30 +86,33 @@ for i in range(len(out_data)):
     phase_error = phase_ideal[i] - phase_real[i]
     print(phase_error)
 
-#Plot time domain and spectrogram
-
-#plt.subplot(2,1,1)
-#plt.plot(time_domain, out_data)
-#plt.xlabel("Time [sec]")
-#plt.ylabel("Magnitude")
-
-#plt.subplot(2,1,2)
-#plt.pcolormesh(f, t, Sxx, shading="gouraud")
-#plt.xlabel("Freqeuency [Hz]")
-#plt.ylabel("Time [sec]")
-#plt.show()
-
-# This is for testing only
-print(offset)
-samples_of_f_1 = np.abs(np.fft.fftshift(np.fft.fft((samples_shifted))))
-fig, (plotT, plotF) = plt.subplots(2)
-plotT.plot(freq_domain,  samples_of_f_1)
-plotF.plot(freq_domain, data_array_fft_shifted)
-#plt.show()
-
-#Fine Frequency Correction
-
-#DPLL
 #LPF
 #NCO
 
+#Demo from Wyglinski
+# fsk_dpll.py
+# 
+# Demo of simple DPLL module operating on simulated FSK data
+#
+# A. M. Wyglinski (alexw@wpi.edu), 2021.03.06
+# Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+# Define radio parameters
+Rsymb = 1e6  # BLE symbol rate
+Rsamp = 20.0e6 # Sampling rate
+N = int(1e3)  # Total number of signal samples in demo
+Foffset = 1.0e6  # Expected frequency offset of FSK tones from signal carrier frequency (Hz)
+PhaseOffset = 0.0  # Initial phase offset of FSK modulation (radians)
+# Generate time indices
+t = np.linspace(0.0,(N-1)/(float(Rsamp)),N)  
+# Generate ideal I/Q signal constellation points without unexpected frequency offset
+deltaF = 0.0 # Unexpected frequency offset set to zero
+dataI = np.cos(2.0*np.pi*(Foffset+deltaF)*t+PhaseOffset*np.ones(N)) # Inphase data samples
+dataQ = -np.sin(2.0*np.pi*(Foffset+deltaF)*t+PhaseOffset*np.ones(N)) # Quadrature data samples
+# Plot signal constellation diagram
+plt.figure(figsize=(9, 5))
+plt.plot(dataI,dataQ)
+plt.xlabel('Inphase')
+plt.ylabel('Quadrature')
+plt.show()
