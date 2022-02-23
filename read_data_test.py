b9b7fee6a916ebf3b2718fc7df9d0b658855da74
Jorgo Gushi <89947053+jorgogushi@users.noreply.github.com>
2022-02-23 14:20:51 -0500

    Update read_data_test.py

--- read_data_test.py
+++ read_data_test.py
@@ -10,11 +10,11 @@ from scipy.signal import butter,lfilter, freqz, filtfilt
 data_array = np.loadtxt("/Users/jorgogushi/Desktop/data_array_20fs_2426fc_1", dtype=np.cdouble)
 
 
-sample_rate = 40e6 #Hz
+sample_rate = 20e6 #Hz
 fc = 2426e6 #Hz
-buffer_size = 2**11
+buffer_size = 2**12
 start = int(8e5)
-end = int(8e5+buffer_size-1)
+end = int(8e5+buffer_size)
 print(start)
 print(end)
 data_array = data_array[start:end]
@@ -87,13 +87,12 @@ for i in range(len(samples_shifted)):
     print(i)
     for j in range(len(samples_shifted)):
         phase[j] = np.angle(samples_shifted[j])
-        if -np.pi<phase[j]<np.pi/2:
+        if -np.pi<phase[j]<-np.pi/2:
             phase[j] = -np.pi - phase[j]
-        if np.pi<phase[j]<np.pi/2:
+        if np.pi/2<phase[j]<np.pi:
             phase[j] = np.pi - phase[j]
-    weighedAverage = phase[i-1]*0.4 + phase[i-2]*0.3 + phase[i-3]*0.2 + phase[i-4]*0.1
-    samples_of_dpll[i] = samples_shifted[i] * np.exp(-1j*2*np.pi*weighedAverage)
-
+    avg = phase[i-1]*0.4 + phase[i-2]*0.3 + phase[i-3]*0.2 + phase[i-4]*0.1
+    samples_of_dpll[i] = samples_shifted[i] * np.exp(-1j*2*np.pi*avg)
 
 #Plotting
 plt.scatter(np.real(samples_shifted), np.imag(samples_shifted), marker = '.')
@@ -106,7 +105,68 @@ plt.title('Time Domain vs. Phase')
 plt.show()
 
 # Change in Phase
-change_in_phase = np.zeros(len(samples_shifted))
+phase_array = np.zeros(len(samples_of_dpll))
 
-for k in range(len(samples_shifted)):
-    change_in_phase[k] = np.angle(samples_shifted[k])
+for k in range(len(samples_of_dpll)):
+    phase_array[k] = np.angle(samples_of_dpll[k])
+    if phase_array[k]<0:
+        phase_array[k]=phase_array[k]+2*np.pi 
+    phase_array[k] = phase_array[k] - phase_array[k-1]
+
+plt.plot(time_domain,phase_array)
+plt.show()
+
+#p_change = np.zeros(len(samples_of_dpll))
+#for m in range(p_change[1:len(p_change)]):
+#    p_change = phase_array[k]-phase_array[k-1]
+
+#plt.scatter(time_domain,p_change,marker = 'x')
+#plt.show()
+
+# Lowpass Filter (LPF)
+
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+def butter_lowpass_filter(phase_array, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, phase_array)
+    return y
+
+
+# Setting standard filter requirements.
+order = 1
+fs = sample_rate       
+cutoff = 3.667  
+
+b, a = butter_lowpass(cutoff, fs, order)
+
+# Plotting the frequency response.
+w, h = freqz(b, a, worN=8000)
+plt.subplot(2, 1, 1)
+plt.plot(0.5*fs*w/np.pi, np.abs(h), 'b')
+plt.plot(cutoff, 0.5*np.sqrt(2), 'ko')
+plt.axvline(cutoff, color='k')
+plt.xlim(0, 0.5*fs)
+plt.title("Lowpass Filter Frequency Response")
+plt.xlabel('Frequency [Hz]')
+plt.grid()
+
+
+# Filtering and plotting
+y = butter_lowpass_filter(phase_array, cutoff, fs, order)
+
+plt.subplot(2, 1, 2)
+plt.plot(time_domain, phase_array, 'b-', label='data')
+plt.plot(time_domain, y, 'g-', linewidth=2, label='filtered data')
+plt.xlabel('Time [sec]')
+plt.grid()
+plt.legend()
+
+plt.subplots_adjust(hspace=0.35)
+plt.show()
+
+
